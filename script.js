011613357365c26daa50c4eb3bad94ebc06d5c06
FernaoFerreira <fernaoqf@gmail.com>
2025-06-06 13:54:36 -0300

    Solved bug that breaks the grid square format

--- script.js
+++ script.js
@@ -2,19 +2,18 @@
 const sq = document.createElement('div');
 sq.classList.add("square");
 let container = document.querySelector('#container');
-
-
-
+const bttn = document.getElementById('clear');
 
 //fills the height and width with squares
-let size = 16;
-container.style.setProperty('--grid-size', size);
+let size = 32;
 
 
 function fillArea(size){
+    container.style.setProperty('--grid-size', size);
     for(let i = 0; i<(size * size); i++){
         container.appendChild(sq.cloneNode(true));
     }
+    
 }
 
 fillArea(size);
@@ -26,4 +25,14 @@ container.addEventListener("mouseover", event=>{
 }
 )
 
-
+bttn.addEventListener("click", event =>{
+    size = prompt("Enter number of squares per side (max 100)");
+    if(size > 100){
+        alert("Grid could not be created!");
+    }
+    else{
+        container.textContent = '';
+        fillArea(size);
+    }
+ 
+})
